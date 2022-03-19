3f13a67cf10e3518a8fc80e2b3b3aac708bbdd0c
Hubert Wo <2518652+HubertWo@users.noreply.github.com>
2022-03-19 09:54:37 +0100

    Empty Maven project

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Creating release with Maven
+This repository contains source code for blog post
+[Creating release with Maven](TODO). \
+If you found this code or post useful do not forget to leave a ⭐️  Thanks! :)
+
---
+++ src/main/java/codes/hubertwo/maven/release/App.java
@@ -0,0 +1,10 @@
+package codes.hubertwo.maven.release;
+
+import static java.lang.String.format;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println(format("Hello! Application version is: %s", "1.0-SNAPSHOT"));
+    }
+}
