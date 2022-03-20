fa799ea2bf6fbe9f912318427f6deaf2200f4854
Hubert Wo <2518652+HubertWo@users.noreply.github.com>
2022-03-20 12:51:13 +0100

    Release and shade plugin

--- src/main/java/codes/hubertwo/maven/release/App.java
+++ src/main/java/codes/hubertwo/maven/release/App.java
@@ -1,10 +1,29 @@
 package codes.hubertwo.maven.release;
 
-import static java.lang.String.format;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.util.jar.Manifest;
+
+import static java.lang.System.out;
 
 public class App {
+    private static final String NO_MANIFEST_DETAILS_MESSAGE = "No manifest file found. Did you build the package using Maven?";
 
-    public static void main(String[] args) {
-        System.out.println(format("Hello! Application version is: %s", "1.0-SNAPSHOT"));
+    public static void main(String[] args) throws Exception {
+        out.println("Hello! Application release details:");
+        out.println(getManifest());
+    }
+
+    private static String getManifest() throws Exception {
+        try (InputStream manifestStream = App.class.getClassLoader().getResourceAsStream("META-INF/MANIFEST.MF")) {
+            if (manifestStream != null) {
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                new Manifest(manifestStream).write(out);
+                String manifestDetails = out.toString();
+                return manifestDetails.isBlank() ? NO_MANIFEST_DETAILS_MESSAGE : manifestDetails;
+            }
+        }
+
+        return NO_MANIFEST_DETAILS_MESSAGE;
     }
 }
